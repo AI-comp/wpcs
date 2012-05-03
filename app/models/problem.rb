86c2a975e563743da5dcc0ba887b8ad4626eb673
yaguchi <murooka.1129.x50@gmail.com>
2012-05-03 20:45:47 +0900

    problemモデルに必要なフィールドを追加し、それに合わせでviewを変更

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -7,5 +7,13 @@ class Problem
   field :content
   field :input
   field :output
+  field :limit
+  field :sample_input
+  field :sample_output
+  field :small_input
+  field :small_answer
+  field :large_input
+  field :large_answer
+  field :published, type: Boolean, default: false
 
 end
