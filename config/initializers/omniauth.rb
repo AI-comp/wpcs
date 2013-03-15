3b6be0b41104c2170dbc2e3ba21ddda001a4df7b
Genki Sugimoto <cfhoyuk.reccos.nelg@gmail.com>
2013-03-15 18:18:00 +0900

    Use google's anonymous authentication

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :developer unless Rails.env.production?
+  # provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :google, "anonymous", "anonymous"
+end
