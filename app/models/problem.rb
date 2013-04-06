4ebc9ede95584269640ffb1e0f19d63ace4a1531
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-04-07 00:56:45 +0900

    Add some methods to model and remove unused fields

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,21 +1,30 @@
+require 'redcarpet'
+
 class Problem
 
   include Mongoid::Document
   include Mongoid::Timestamps
 
   field :title
-  field :content_path
+  field :description
+  field :description_html
   field :small_input
   field :small_output
   field :large_input
   field :large_output
   field :small_score, type: Integer
   field :large_score, type: Integer
-  field :is_solved, type: Boolean, default: false
-
 
   belongs_to :contest
   has_many :submits
+
+  before_save :convert_html
+
+
+  def convert_html
+    md = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    self.description_html = md.render(self.description)
+  end
 
   def correct?(answer, problem_type)
     if problem_type=='small'
