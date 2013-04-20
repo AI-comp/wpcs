4d75a48a810eccd1e99b76965835ce16cb936297
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-04-20 18:01:57 +0900

    Enable some markdown options

--- lib/autoload/markdown.rb
+++ lib/autoload/markdown.rb
@@ -3,7 +3,16 @@ require 'redcarpet'
 class Markdown
 
   def self.to_html(md)
-    Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(md)
+    Redcarpet::Markdown.new(Redcarpet::Render::HTML, {
+      no_intra_emphasis: true,
+      tables: true,
+      fenced_code_blocks: true,
+      autolink: true,
+      strikethrough: true,
+      lax_spacing: true,
+      space_after_headers: true,
+      superscript: true,
+    }).render(md)
   end
 
 end
