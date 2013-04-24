9f12109004fdfe4bda4f0f358ff2e356db218ede
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-04-25 03:40:23 +0900

    Add spec of Admin::UsersController

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -2,4 +2,37 @@ require 'spec_helper'
 
 describe Admin::UsersController do
 
+  describe 'GET /index' do
+
+    before do
+      get :index
+      # TODO
+      # create some users
+    end
+
+    it 'should return 200 status' do
+      expect(response).to be_success
+    end
+
+    it 'should show all users' do
+      users = assigns[:users]
+      expect(users).not_to be_nil
+      # TODO
+      # check the size of users
+    end
+
+  end
+
+  describe 'GET /new' do
+
+    before do
+      get :new
+    end
+
+    it 'should return 200 status' do
+      expect(response).to be_success
+    end
+
+  end
+
 end
