b71d58b81393de38a8b47c8bc5679e984a6b4ee3
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-04-25 03:44:04 +0900

    Add spec of Admin::ContestsController

--- spec/controllers/admin/contests_controller_spec.rb
+++ spec/controllers/admin/contests_controller_spec.rb
@@ -2,4 +2,28 @@ require 'spec_helper'
 
 describe Admin::ContestsController do
 
+  describe 'GET /index' do
+
+    before do
+      get :index
+    end
+
+    it 'should return 200 status' do
+      expect(response).to be_success
+    end
+
+  end
+
+  describe 'GET /new' do
+
+    before do
+      get :new
+    end
+
+    it 'should return 200 status' do
+      expect(response).to be_success
+    end
+
+  end
+
 end
