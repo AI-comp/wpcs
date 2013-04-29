f498a90ee3e26e7fe9f77285cb41317d2ec4c1fc
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-04-30 03:18:55 +0900

    Fix specs for #71

--- spec/controllers/admin/contests_controller_spec.rb
+++ spec/controllers/admin/contests_controller_spec.rb
@@ -6,36 +6,124 @@ describe Admin::ContestsController do
 
   describe 'GET /index' do
 
-    it 'should respond success' do
+    it 'should not respond success' do
       get :index
-      expect(response).to be_success
+      expect(response).not_to be_success
+    end
+
+    describe 'when contestant logged in' do
+
+      include_examples 'login contestant'
+
+      it 'should not respond success' do
+        get :index
+        expect(response).not_to be_success
+      end
+
+    end
+
+    describe 'when admin logged in' do
+
+      include_examples 'login admin'
+
+      it 'should respond success' do
+        get :index
+        expect(response).to be_success
+      end
+
     end
 
   end
 
   describe 'GET /new' do
 
-    it 'should respond success' do
+    it 'should not respond success' do
       get :new
-      expect(response).to be_success
+      expect(response).not_to be_success
+    end
+
+    describe 'when contestant logged in' do
+
+      include_examples 'login contestant'
+
+      it 'should not respond success' do
+        get :new
+        expect(response).not_to be_success
+      end
+
+    end
+
+    describe 'when admin logged in' do
+
+      include_examples 'login admin'
+
+      it 'should respond success' do
+        get :new
+        expect(response).to be_success
+      end
+
     end
 
   end
 
   describe 'GET /show/:id' do
 
-    it 'should respond success' do
+    it 'should not respond success' do
       get :show, id: contest
-      expect(response).to be_success
+      expect(response).not_to be_success
+    end
+
+    describe 'when contestant logged in' do
+
+      include_examples 'login contestant'
+
+      it 'should not respond success' do
+        get :show, id: contest
+        expect(response).not_to be_success
+      end
+
+    end
+
+    describe 'when admin logged in' do
+
+      include_examples 'login admin'
+
+      it 'should respond success' do
+        get :show, id: contest
+        expect(response).to be_success
+      end
+
     end
 
   end
 
   describe 'GET /edit/:id' do
 
-    it 'should respond success' do
+    it 'should not respond success' do
       get :edit, id: contest
-      expect(response).to be_success
+      expect(response).not_to be_success
+    end
+
+    describe 'when contestant logged in' do
+
+      include_examples 'login contestant'
+
+      it 'should not respond success' do
+        get :edit, id: contest
+        expect(response).not_to be_success
+      end
+
+    end
+
+    describe 'when admin logged in' do
+
+      include_examples 'login admin'
+
+      it 'should respond success' do
+        get :edit, id: contest
+        expect(response).to be_success
+      end
+
     end
 
   end
