f753a1b3a92ddb592b9d561974a76e83b2b1af56
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-04-30 03:18:01 +0900

    Fix spec helpers

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,14 +1,29 @@
 shared_examples_for 'login contestant' do
 
-  let(:user) { create(:contestant) }
+  let(:contestant) { create(:contestant) }
 
   before do
-    controller.login_user(user)
+    controller.login_user(contestant)
   end
 
   after do
     controller.logout_user
-    user.destroy
+    contestant.destroy
+  end
+
+end
+
+shared_examples_for 'login admin' do
+
+  let(:admin) { create(:admin) }
+
+  before do
+    controller.login_user(admin)
+  end
+
+  after do
+    controller.logout_user
+    admin.destroy
   end
 
 end
