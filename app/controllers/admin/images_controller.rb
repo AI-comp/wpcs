1e1e4c8a23ed768a841b2ca0befb749350deab86
Naoki YAGUCHI <murooka.1129.x50@gmail.com>
2013-05-01 00:15:54 +0900

    Remove useless code
    No one should be able to get contents of admin page by JSON

--- app/controllers/admin/images_controller.rb
+++ app/controllers/admin/images_controller.rb
@@ -1,35 +1,18 @@
 class Admin::ImagesController < AdminController
+
   # GET /images
-  # GET /images.json
   def index
     @images = Image.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @images }
-    end
   end
 
   # GET /images/1
-  # GET /images/1.json
   def show
     @image = Image.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @image }
-    end
   end
 
   # GET /images/new
-  # GET /images/new.json
   def new
     @image = Image.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @image }
-    end
   end
 
   # GET /images/1/edit
@@ -38,46 +21,38 @@ class Admin::ImagesController < AdminController
   end
 
   # POST /images
-  # POST /images.json
   def create
     @image = Image.new(params[:image])
 
     respond_to do |format|
       if @image.save
         format.html { redirect_to admin_image_path(@image), notice: 'Image was successfully created.' }
-        format.json { render json: @image, status: :created, location: @image }
       else
         format.html { render action: "new" }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PUT /images/1
-  # PUT /images/1.json
   def update
     @image = Image.find(params[:id])
 
     respond_to do |format|
       if @image.update_attributes(params[:image])
         format.html { redirect_to admin_image_path(@image), notice: 'Image was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: "edit" }
-        format.json { render json: @image.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /images/1
-  # DELETE /images/1.json
   def destroy
     @image = Image.find(params[:id])
     @image.destroy
 
     respond_to do |format|
       format.html { redirect_to images_url }
-      format.json { head :no_content }
     end
   end
 end
