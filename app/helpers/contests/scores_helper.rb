c8aefb1a9feb1c5a6be726910c5dacff64e9cdda
SAKAMOTO Kazunori <exkazuu@gmail.com>
2013-05-05 09:35:38 -0700

    Fix visible groups. Sort groups by scores. Close #150 Close #162

--- app/helpers/contests/scores_helper.rb
+++ app/helpers/contests/scores_helper.rb
@@ -1,9 +1,7 @@
 module Contests::ScoresHelper
   def groups_to_show
-    if @current_user.group.users.any? { |u| u.is_admin? }
-      Group.each.select { |g| g.users.any? { |u| u.is_admin? } }
-    else
-      Group.each.select { |g| g.users.all? { |u| !u.is_admin? } }
-    end
+    groups = Group.each
+    groups = groups.select { |g| g.users.all? { |u| not u.is_admin? } } unless @current_user.is_admin?
+    groups.sort { |a, b| a.score_for(@contest) <=> b.score_for(@contest) }
   end
 end
