3b59dfdab6826080781ca743a41fa215840de0ab
SAKAMOTO Kazunori <exkazuu@gmail.com>
2013-05-05 10:18:07 -0700

    Refactor scores_helper.

--- app/helpers/contests/scores_helper.rb
+++ app/helpers/contests/scores_helper.rb
@@ -1,7 +1,6 @@
 module Contests::ScoresHelper
   def groups_to_show
-    groups = Group.each
-    groups = groups.select { |g| g.users.all? { |u| not u.is_admin? } } unless @current_user.is_admin?
-    groups.sort { |a, b| a.score_for(@contest) <=> b.score_for(@contest) }
+    groups = @current_user.is_admin? ? Group.each : Group.each.select { |g| not g.include_admin? }
+    groups.sort_by { |group| group.score_for( @contest ) }
   end
 end
