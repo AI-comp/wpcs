76bd945c6fe7ef1d3cd92fb35e468be2ea99e26a
Kazunori Sakamoto <exkazuu@gmail.com>
2013-10-31 20:37:31 +0900

    Remove failing test

--- spec/features/performance_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "the signup process", :type => :feature do
-  it "login" do
-    visit '/'
-    fill_in 'uid', :with => 'user_1_1'
-    fill_in 'password', :with => 'password'
-    click_on 'login'
-    page.should have_content 'Logout'
-  end
-end
