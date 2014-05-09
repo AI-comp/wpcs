5673724cd84917621d7ace39b52036bae102dd30
Kazunori SAKAMOTO <exkazuu@gmail.com>
2014-05-09 01:36:38 +0000

    Enhance performance test.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,35 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 
-# FIXME: This is dirty because it's copy from lib/tasks/populate.rake
 # Populate database
 puts "Creating a contest with some problems"
 FactoryGirl.create(:contest, save_problems: true)
-puts "Creating 10 groups"
-FactoryGirl.create_list(:group, 10)
-puts "Creating 100 contestants"
-FactoryGirl.create_list(:contestant, 100)
+puts "Creating 40 groups"
+FactoryGirl.create_list(:group, 40)
+puts "Creating 200 contestants"
+FactoryGirl.create_list(:contestant, 200)
 puts "Make users attend the contest"
+puts "Solve some problems"
 contest = Contest.first
+problems = contest.problems.to_a
 User.all.each do |user|
-  user.attend(contest)
+  attend = user.attend(contest)
+  pcount = (1...problems.count).to_a.sample
+  problems.shuffle!
+  # Submit some wrong answers
+  problems.take(pcount).each do |p|
+    Problem.types.each do |type|
+      (0...2).to_a.sample.times do 
+        attend.submit(p, 'dummy', type)
+      end
+    end
+  end
+  # Submit some correct answers
+  problems.take(pcount).each do |p|
+    tcount = (1...Problem.types.count).to_a.sample
+    Problem.types.take(tcount).each do |type|
+      ans = p.output(type)
+      attend.submit(p, ans, type)
+    end
+  end
 end
