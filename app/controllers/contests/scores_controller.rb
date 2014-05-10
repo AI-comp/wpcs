05369b70682b1a65fdfcf917d8b5905fc6b78e68
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2014-05-10 19:46:32 +0900

    Optimize query in Scoreboard

--- app/controllers/contests/scores_controller.rb
+++ app/controllers/contests/scores_controller.rb
@@ -13,6 +13,31 @@ class Contests::ScoresController < ApplicationController
   # GET /contests/1/score
   # GET /contests/1/score.json
   def show
+    groups = @current_user.is_admin? ? Group.all : Group.all.reject(&:include_admin?)
+    attendance_ids = []
+    gid_from_aid = {}
+    groups.each do |g|
+      atts = Attendance.where(user_id: g.user_ids).select(:id)
+      attendance_ids += atts.map { |a| a.id }
+      atts.each { |a| gid_from_aid[a.id] = g.id }
+    end
+
+    submissions = Submission.where(
+      problem_id: @contest.problem_ids,
+      solved: true,
+      attendance_id: attendance_ids
+    )
+
+    @submissions_table = {}
+    submissions.each do |s|
+      gid = gid_from_aid[s.attendance_id]
+      pid = s.problem_id
+      type = s.problem_type
+      @submissions_table[gid] ||= {}
+      @submissions_table[gid][pid] ||= {}
+      @submissions_table[gid][pid][type] = s
+    end
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @problems }
--- app/helpers/contests/scores_helper.rb
+++ app/helpers/contests/scores_helper.rb
@@ -1,10 +1,13 @@
 module Contests::ScoresHelper
   def groups_to_show(contest)
     groups = @current_user.is_admin? ? Group.all : Group.all.reject(&:include_admin?)
-    groups = groups.select { |group|
-      group.users.any? { |user|
-        user.attendances.any? { |att|
-          att.contest == contest } } }
+    groups = groups.select do |group|
+      group.users.any? do |user|
+        user.attendances.any? do |att|
+          att.contest == contest
+        end
+      end
+    end
     groups.sort_by { |group| -group.score_for( @contest ) }
   end
 
