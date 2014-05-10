d6addd7c8673eb6813d4a9657fd2d5a18b6556f1
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2014-05-10 19:52:22 +0900

    Refactor ScoresController

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -25,4 +25,35 @@ class Contest < ActiveRecord::Base
     end_time < Time.now
   end
 
+  # returns: table[group_id][problem_id][type] => submission
+  def all_submissions_table(include_admin = false)
+    groups = include_admin ? Group.all : Group.all.reject(&:include_admin?)
+
+    attendance_ids = []
+    gid_from_aid = {}
+    groups.each do |g|
+      atts = Attendance.where(user_id: g.user_ids).select(:id)
+      attendance_ids += atts.map { |a| a.id }
+      atts.each { |a| gid_from_aid[a.id] = g.id }
+    end
+
+    submissions = Submission.where(
+      problem_id: problem_ids,
+      solved: true,
+      attendance_id: attendance_ids
+    )
+
+    submissions_table = {}
+    submissions.each do |s|
+      gid = gid_from_aid[s.attendance_id]
+      pid = s.problem_id
+      type = s.problem_type
+      submissions_table[gid] ||= {}
+      submissions_table[gid][pid] ||= {}
+      submissions_table[gid][pid][type] = s
+    end
+
+    submissions_table
+  end
+
 end
