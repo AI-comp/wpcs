d56740cfc2ae2a278e0bcb6e1d16291250ca8ae2
ss0201 <ss0201.777@gmail.com>
2014-05-15 22:38:01 +0900

    Change: keep image format

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,7 +3,6 @@ class Image < ActiveRecord::Base
   attr_accessible :attachment
   has_attached_file :attachment,
     :styles => {
-      :original  => ['600x400', :jpg],
       :thumbnail => ['160x160', :jpg]
     }
   validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
